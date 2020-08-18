47542dfdf73b292cd667f95aed35886e66c8e4b0
Michael Menebraye <56199702+hummingbed@users.noreply.github.com>
2020-08-18 11:08:49 +0100

    Update path.js
    Removed

--- Bakery_task/path.js
+++ Bakery_task/path.js
@@ -1,17 +1,1 @@
-let colors = ['black','white','black'];
 
-let button = document.getElementById('sun');
-
-button.addEventListener('click', function(){
-    var randomColor = colors[Math.floor(Math.random() * colors.length)]
-
-    let container = document.getElementById('house');
-
-    container.style.background = randomColor;
-});
-
-$(document).ready(function(){
-    $('#button-two').click(function(){
-      $('#box-two').css("transform","translateY(250px)");
-    });
-  });
